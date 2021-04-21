19fe5f1f3ee9c638c49a5f838fb2de373b4fb61f
Adrian Sarca <sarcaadi@gmail.com>
2021-04-21 13:02:29 +0300

    design

--- js-movie-app/main.js
+++ js-movie-app/main.js
@@ -13,6 +13,12 @@ const title = document.querySelector('.title');
 const pCards = document.querySelector('.pCards');
 const tCards = document.querySelector('.tCards');
 const uCards = document.querySelector('.uCards');
+const trCards = document.querySelector('.trCards');
+const day = document.querySelector('#day');
+const week = document.querySelector('#week');
+const pTheatres = document.querySelector('#pTheatres');
+const pTv = document.querySelector('#pTv');
+const trending = document.querySelector('#trending');
 
 
 
@@ -27,7 +33,7 @@ function movieSection(movies){
         <div class = cardInside>
         <img src="${IMG_URL + movie.poster_path}" data-movie-id ="${movie.id}" >
         <div class= "circle">${movie.vote_average}</div>
-        <div class= "cardTitle">${movie.title}</div>
+        <div class= "cardTitle">${movie.title ? movie.title : movie.name}</div>
         </div>
         `
         }
@@ -79,15 +85,61 @@ btnSearch.onclick = function (){
 
 
 function loadPopular(){
+    pCards.innerHTML = '';
     let popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
     fetch(popularUrl)
         .then((res) => res.json())
         .then((data) =>{
+            
             const movies = data.results;
             const movieBlock = createMovieSection(movies);
             pCards.appendChild(movieBlock);
         })
 }
+
+
+function loadTrendingDay(){
+    const poster = 'https://image.tmdb.org/t/p/original/';
+    trCards.innerHTML = '';
+    let dayUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${api_key}`;
+    fetch(dayUrl)
+        .then((res) => res.json())
+        .then((data) =>{           
+            const movies = data.results;
+            trending.style.backgroundImage = `url(${poster + movies[Math.floor(Math.random() * 20)].poster_path})`
+
+            const movieBlock = createMovieSection(movies);
+            trCards.appendChild(movieBlock);
+        })
+}
+
+function loadTrendingWeek(){
+    const poster = 'https://image.tmdb.org/t/p/original/';
+    trCards.innerHTML = '';
+    let weekUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}`;
+    fetch(weekUrl)
+        .then((res) => res.json())
+        .then((data) =>{
+            const movies = data.results;
+            const movieBlock = createMovieSection(movies);
+            trCards.appendChild(movieBlock);
+        })
+}
+
+
+function loadPopularTv(){
+    pCards.innerHTML ='';
+    let popularUrlTv = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=en-US&page=1`;
+    fetch(popularUrlTv)
+        .then((res) => res.json())
+        .then((data) =>{
+            const movies = data.results;
+            console.log(movies);
+            const movieBlock = createMovieSection(movies);
+            pCards.appendChild(movieBlock);
+        })
+}
+
 
 function loadTop(){
     let popularUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`;
@@ -113,7 +165,33 @@ function loadNext(){
 }
 
 
+pTheatres.onclick = function(){
+    loadPopular();
+    pTheatres.classList.add("active");
+    pTv.classList.remove("active");
+}
 
+pTv.onclick = function(){
+    loadPopularTv();
+    pTv.classList.add("active");
+    pTheatres.classList.remove("active");
+}
+
+day.onclick = function(){
+    loadTrendingDay();
+    day.classList.add("active");
+    week.classList.remove("active");
+}
+
+week.onclick = function(){
+    loadTrendingWeek();
+    week.classList.add("active");
+    day.classList.remove("active");
+}
+
+
+
+loadTrendingDay();
 loadNext();
 loadTop();
 loadPopular();
